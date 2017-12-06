f14c03b84b27a761e45c03e349b1ca28e50cf6b0
Konds Sonawane <kondya@Kondirams-MacBook-Air.local>
2017-12-06 22:48:14 +0530

    demo

--- SplashScreenDemo/ViewController.m
+++ SplashScreenDemo/ViewController.m
@@ -9,20 +9,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    float i;
+    NSTimer *timer;
+}
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    NSArray *tintColors = @[[UIColor whiteColor]];
+    
+    _progressBarRoundedFat.progressTintColors       = tintColors;
+    _progressBarRoundedFat.stripesOrientation       = YLProgressBarStripesOrientationLeft;
+    _progressBarRoundedFat.indicatorTextDisplayMode = YLProgressBarIndicatorTextDisplayModeProgress;
+    _progressBarRoundedFat.indicatorTextLabel.font  = [UIFont fontWithName:@"Arial-BoldMT" size:20];
+    _progressBarRoundedFat.progressStretch          = NO;
+    _progressBarRoundedFat.type               = YLProgressBarTypeFlat;
+    i=0.1;
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(timerCalled) userInfo:nil repeats:YES];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)timerCalled
+{
+    i=i+0.01;
+    [self setProgress:i animated:YES];
+    NSLog(@"Timer Called %f",i);
+    if((int)i==1)
+    {
+        [timer invalidate];
+        [self performSegueWithIdentifier:@"abc" sender:nil];
+    }
+    
+}
+- (void)setProgress:(CGFloat)progress animated:(BOOL)animated
+{
+    
+    [_progressBarRoundedFat setProgress:progress animated:animated];
 }
 
 
---
+++ SplashScreenDemo/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  SplashScreenDemo
+//
+//  Created by Kondya on 06/12/17.
+//  Copyright © 2017 Kondya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
