6b908d89fd9436cba5992d9b1bd6974ed18357d7
Konds Sonawane <kondya@Kondirams-MacBook-Air.local>
2017-12-06 21:29:24 +0530

    Initial Commit

---
+++ SplashScreenDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SplashScreenDemo
+//
+//  Created by Kondya on 06/12/17.
+//  Copyright © 2017 Kondya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
